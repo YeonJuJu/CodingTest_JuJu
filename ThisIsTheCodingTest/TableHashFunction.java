866a945717ae8ee70f9c867e8790e3d36af45e58
YeonJuJu <uplay_98@naver.com>
2022-12-30 11:22:28 +0900

    Update TableHashFunction.java

--- ThisIsTheCodingTest/TableHashFunction.java
+++ ThisIsTheCodingTest/TableHashFunction.java
@@ -1,16 +1,12 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class TableHashFunction {
     public static void main(String[] args){
-        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int[][] data = {{2,2,6}, {1,5,10}, {4,2,9}, {3,8,3}};
         int col = 2;
         int row_begin = 2;
-        int end_begin = 2;
+        int end_begin = 3;
 
         System.out.println(solution(data, col, row_begin, end_begin));
     }
@@ -28,30 +24,14 @@ public class TableHashFunction {
             }
         });
 
-        for(int i=0;i<data.length;i++){
-            int[] tmp = data[i];
-            for(int j=0;j<data[i].length;j++){
-                System.out.print(tmp[j] + " ");
-            }
-            System.out.println();
-        }
-
-        int[] sArr = new int[data.length];
-
         for(int i=row_begin ; i<=row_end; i++){
             int total = 0;
-            int[] tmp = data[i];
+            int[] tmp = data[i-1];
             for(int j=0;j< tmp.length; j++){
-                total += tmp[j]%(i+1);
+                total += tmp[j]%(i);
             }
-            sArr[i] = total;
+            answer = answer ^ total;
         }
-
-        for(int i=row_begin ; i<=row_end; i++){
-            System.out.println(sArr[i]);
-        }
-
-        //xor을 어떻게 하지 ?
 
         return answer;
     }
